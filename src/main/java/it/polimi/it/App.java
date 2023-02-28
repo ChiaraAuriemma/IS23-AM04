fbf917bea6708dc09d57f3c378fd8a5ab05ea3ae
Benels <francesco.benelle@mail.polimi.it>
2023-02-28 21:41:25 +0100

    prova

--- src/main/java/it/polimi/it/App.java
+++ src/main/java/it/polimi/it/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args ) {
         System.out.println("Hello World!");
         System.out.ptintl("secondo commit");
+        System.out.println("third commit (f)");
     }
 }
