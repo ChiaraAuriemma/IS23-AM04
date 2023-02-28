dc1e4fb406732a76c21833e6c9f1d9e1458261ba
Alberto <alberto1.cavallotti@mail.polimi.it>
2023-02-28 17:06:17 +0100

    second commit

--- src/main/java/it/polimi/it/App.java
+++ src/main/java/it/polimi/it/App.java
@@ -6,8 +6,8 @@ package it.polimi.it;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println("Hello World!");
+        System.out.ptintl("secondo commit");
     }
 }
