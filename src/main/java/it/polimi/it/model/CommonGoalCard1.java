d70cbc7e9910ee7220c2150511110b4535c835c7
ChiaraAuriemma <chiara.auriemma@mail.polimi.it>
2023-03-16 23:20:17 +0100

    add CommonGoalCard

---
+++ src/main/java/it/polimi/it/model/CommonGoalCard1.java
@@ -0,0 +1,13 @@
+package it.polimi.it.model;
+
+public class CommonGoalCard1 extends CommonGoalCard{
+
+    public CommonGoalCard1(){
+        super();
+        this.id = 1;
+    }
+
+    public Boolean CheckGoal(){
+
+    }
+}
