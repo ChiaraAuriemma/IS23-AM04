771664564c2ac8fde93eb500e6970fee4b62a693
ChiaraAuriemma <chiara.auriemma@mail.polimi.it>
2023-03-18 12:03:56 +0100

    upadate PersonalGoalCard

--- src/main/java/it/polimi/it/model/PersonalGoalCard.java
+++ src/main/java/it/polimi/it/model/PersonalGoalCard.java
@@ -21,7 +21,10 @@ public abstract class PersonalGoalCard extends Card{
         this.name = "Personal";
         this.NumCompletedTasks = 0;
         this.CheckColor = new ArrayList<Integer>(6);
-        //this.CheckColor.addAll(0,0) need to check
+        int i;
+        for(i=0; i<6; i++){
+            this.CheckColor.add(i,0);
+        }
         this.Pinkpos = new ArrayList<Integer>(2);
         this.Cyanpos = new ArrayList<Integer>(2);
         this.Yellowpos = new ArrayList<Integer>(2);
@@ -30,29 +33,29 @@ public abstract class PersonalGoalCard extends Card{
         this.Greenpos = new ArrayList<Integer>(2);
     }
 
-    //getCell should return PossibleColor , need to check
+
     public int CheckScore(Shelfie shelfie) {
-        if(CheckColor.get(0) == 0 && shelfie.getCell(Pinkpos.get(0), Pinkpos.get(1)).equals(PossibleColors.PINK)){
+        if(CheckColor.get(0) == 0 && shelfie.getCell(Pinkpos.get(0), Pinkpos.get(1)).equals("PINK")){
             NumCompletedTasks++;
             CheckColor.add(0,1);
         }
-        if(CheckColor.get(1) == 0 && shelfie.getCell(Cyanpos.get(0), Cyanpos.get(1)).equals(PossibleColors.CYAN)){
+        if(CheckColor.get(1) == 0 && shelfie.getCell(Cyanpos.get(0), Cyanpos.get(1)).equals("CYAN")){
             NumCompletedTasks++;
             CheckColor.add(1,1);
         }
-        if(CheckColor.get(2)== 0 && shelfie.getCell(Yellowpos.get(0), Yellowpos.get(1)).equals(PossibleColors.YELLOW)){
+        if(CheckColor.get(2)== 0 && shelfie.getCell(Yellowpos.get(0), Yellowpos.get(1)).equals("YELLOW")){
             NumCompletedTasks++;
             CheckColor.add(2,1);
         }
-        if(CheckColor.get(3) == 0 && shelfie.getCell(Bluepos.get(0), Bluepos.get(1)).equals(PossibleColors.BLUE)){
+        if(CheckColor.get(3) == 0 && shelfie.getCell(Bluepos.get(0), Bluepos.get(1)).equals("BLUE")){
             NumCompletedTasks++;
             CheckColor.add(3,1);
         }
-        if(CheckColor.get(4) == 0 && shelfie.getCell(Whitepos.get(0), Whitepos.get(1)).equals(PossibleColors.WHITE)){
+        if(CheckColor.get(4) == 0 && shelfie.getCell(Whitepos.get(0), Whitepos.get(1)).equals("WHITE")){
             NumCompletedTasks++;
             CheckColor.add(4,1);
         }
-        if(CheckColor.get(5) == 0 && shelfie.getCell(Greenpos.get(0), Greenpos.get(1)).equals(PossibleColors.GREEN)){
+        if(CheckColor.get(5) == 0 && shelfie.getCell(Greenpos.get(0), Greenpos.get(1)).equals("GREEN")){
             NumCompletedTasks++;
             CheckColor.add(5,1);
         }
