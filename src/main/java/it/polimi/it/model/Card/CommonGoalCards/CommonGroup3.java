aadd3d2216fb3af4d1090f5f386e49260baf88f3
ChiaraAuriemma <chiara.auriemma@mail.polimi.it>
2023-04-14 10:50:46 +0200

    CommonGroup3 Test

--- src/main/java/it/polimi/it/model/Card/CommonGoalCards/CommonGroup3.java
+++ src/main/java/it/polimi/it/model/Card/CommonGoalCards/CommonGroup3.java
@@ -12,56 +12,60 @@ import java.util.ArrayList;
 
 public class CommonGroup3 extends CommonGoalCard{
 
-    ArrayList<Tile> checked = new ArrayList<Tile>();
+    ArrayList<Tile> checked = new ArrayList<>();
     public CommonGroup3(int id){ //carte 3 e 4
         super(id);
     }
 
     private ArrayList<Tile> adjacent(Shelfie shelfie, int x, int y) {
-        ArrayList<Tile> adjacent = new ArrayList<Tile>();
+        ArrayList<Tile> adjacent = new ArrayList<>();
         if (x > 0) {
             adjacent.add(shelfie.getCell(x-1,y));
         }
         if (y > 0) {
             adjacent.add(shelfie.getCell(x,y-1));
         }
-        if (x < 5) {
+        if (x < 4) {
             adjacent.add(shelfie.getCell(x+1,y));
         }
-        if (y < 6) {
+        if (y < 5) {
             adjacent.add(shelfie.getCell(x,y+1));
         }
         return adjacent;
     }
 
-    private  ArrayList<Tile> recursiveAdjacent(Shelfie shelfie, int x, int y, ArrayList<Tile> checked,  ArrayList<Tile> toVisit) {
+    private  ArrayList<Tile> recursiveAdjacent(Shelfie shelfie, int x, int y, ArrayList<Tile> toVisit, JsonObject jsonObject) {
         int i;
         ArrayList<Tile> tmp = new  ArrayList<>();
         Gson gson = new Gson();
         try {
-            JsonReader reader = new JsonReader(new FileReader("CommonGroup3.json"));
-            JsonArray jsonArray = gson.fromJson(reader, JsonArray.class);
-            for(i=0; jsonArray.get(i).getAsJsonObject().get("type").getAsInt() != id ; i++);
-            JsonObject jsonObject= jsonArray.get(i).getAsJsonObject();
-            if(toVisit.size() == jsonObject.get("numOfTile").getAsInt())
+            if(toVisit.size() == jsonObject.get("numOfTile").getAsInt()){
+                for(Tile tile : toVisit){
+                    if(!checked.contains(tile))
+                        checked.add(tile);
+                }
                 return toVisit;
+            }
 
             ArrayList<Tile> adjacent = adjacent(shelfie, x, y);
+            checked.add(shelfie.getCell(x,y));
             for (Tile tile : adjacent) {
                 if (!checked.contains(tile) && shelfie.getCell(x,y).getColor().equals(shelfie.getCell(tile.getColumn(),tile.getRow()).getColor())) {
                     toVisit.add(tile);
+                    if(toVisit.size() == jsonObject.get("numOfTile").getAsInt()){
+                        checked.add(tile);
+                        return toVisit;
+                    }
                     tmp.add(tile);
-
                 }
             }
-            checked.add(shelfie.getCell(x,y));
 
             for (Tile tile : tmp) {
-                recursiveAdjacent(shelfie, tile.getColumn(), tile.getRow(), checked, toVisit);
+                recursiveAdjacent(shelfie, tile.getColumn(), tile.getRow(), toVisit, jsonObject);
             }
             return toVisit;
         }catch (Exception e){
-           return toVisit;
+           throw new RuntimeException(e);
         }
     }
 
@@ -71,15 +75,15 @@ public class CommonGroup3 extends CommonGoalCard{
         int i,row, column;
         int count=0;
         try{
-            JsonReader reader = new JsonReader(new FileReader("CommonGroup3.json"));
+            JsonReader reader = new JsonReader(new FileReader("src/main/java/it/polimi/it/model/Card/CommonGoalCards/CommonGroup3.json"));
             JsonArray jsonArray = gson.fromJson(reader, JsonArray.class);
             for(i=0; jsonArray.get(i).getAsJsonObject().get("type").getAsInt() != id ; i++);
             JsonObject jsonObject= jsonArray.get(i).getAsJsonObject();
-            for(row=0; row< 5; row++){
-                for(column=0; column<6; column++){
+            for(row=0; row< 6; row++){
+                for(column=0; column<5; column++){
                     ArrayList<Tile> toVisit = new ArrayList<>();
                     if(checked.isEmpty() || !checked.contains(shelfie.getCell(column,row))){
-                        tmp= recursiveAdjacent(shelfie, column, row, checked,toVisit);
+                        tmp= recursiveAdjacent(shelfie, column, row,toVisit, jsonObject);
                         if(tmp.size() == jsonObject.get("numOfTile").getAsInt()){
                             count++;
                         }
@@ -90,7 +94,7 @@ public class CommonGroup3 extends CommonGoalCard{
             }
             return false;
         }catch (Exception e){
-            return false;
+            throw new RuntimeException(e);
         }
 
     }
