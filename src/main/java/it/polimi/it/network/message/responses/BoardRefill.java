a4885fd3cee052613b0b2ce17f9aa52345b73810
Benels <francesco.benelle@mail.polimi.it>
2023-05-18 13:43:03 +0200

    leaderboard

--- src/main/java/it/polimi/it/network/message/responses/BoardRefill.java
+++ src/main/java/it/polimi/it/network/message/responses/BoardRefill.java
@@ -6,6 +6,8 @@ import java.io.Serializable;
 
 public class BoardRefill extends Payload implements Serializable {
 
+    private static final long serialVersionUID = 479614382488698786L;
+
     public BoardRefill(){
 
     }
