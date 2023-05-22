2112eee35e229873b7ac4995710ddefa1ba269ec
jakeu <ballabiogiacomo@gmail.com>
2023-05-22 16:05:07 +0200

    pong

---
+++ src/main/java/it/polimi/it/network/message/others/PongMessage.java
@@ -0,0 +1,9 @@
+package it.polimi.it.network.message.others;
+
+import it.polimi.it.network.message.Payload;
+
+import java.io.Serializable;
+
+public class PongMessage extends Payload implements Serializable {
+
+}
