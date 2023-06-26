21ff2f66046b23726f0934493c4a9d7855b95af4
Alberto Cavallotti <alberto1.cavallotti@mail.polimi.it>
2023-06-26 12:17:57 +0200

    user Test

--- src/test/java/it/polimi/it/model/UserTest.java
+++ src/test/java/it/polimi/it/model/UserTest.java
@@ -1,19 +1,390 @@
 package it.polimi.it.model;
 
+import it.polimi.it.Exceptions.IllegalValueException;
+import it.polimi.it.Exceptions.WrongTileException;
+import it.polimi.it.model.Board.Board;
+import it.polimi.it.model.Tiles.PossibleColors;
+import it.polimi.it.model.Tiles.Tile;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
+    private Game game;
+    private User host;
+    private User joiner;
+    private VirtualViewStub virtualViewStub;
+    private Board board;
+
+    @Before
+    public void setup() {
+        virtualViewStub = new VirtualViewStub();
+        host = new User("Giacomo");
+        joiner = new User("Francesco");
+        game = new Game(2, host, 0, virtualViewStub);
+        game.joinGame(joiner);
+        host.createShelfie();
+    }
+
     @Test
-    public void UserTest(){
-        User u = new User("Alby");
-        //Shelfie shelf = u.createShelfie();
+    public void CorrectInitialization(){
+        User u1 = new User("Giacomo");
 
-        assertNotNull(u);
+        assertNotNull(u1);
+        assertNotNull(u1.getShelfie());
+        assertFalse(u1.getInGame());
+        assertNotNull(u1.getChat());
+    }
 
-        //assertNotNull(shelf);
+    @Test
+    public void MaxValueOfTilesCrashTest() throws IllegalValueException, IOException {
+
+        host.setInGame(false);
+        if(host.maxValueOfTiles() != -1){
+            fail();
+        }
+    }
+
+    @Test
+    public void MaxValueOfTilesTest() throws IllegalValueException, IOException {
+
+        host.setInGame(true);
+
+        assertTrue(host.getInGame());
+
+        if(host.maxValueOfTiles() != 3){
+            fail();
+        }
+
+        host.getShelfie().getShelf()[0][0] = new Tile(0, 0, PossibleColors.BLUE);
+        host.getShelfie().getShelf()[1][0] = new Tile(1,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[2][0] = new Tile(2,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[3][0] = new Tile(3, 0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[4][0] = new Tile(4,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[5][0] = new Tile(5,0,PossibleColors.BLUE);
+
+        host.getShelfie().getShelf()[0][1] = new Tile(0, 1, PossibleColors.BLUE);
+        host.getShelfie().getShelf()[1][1] = new Tile(1,1,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[2][1] = new Tile(2,1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[3][1] = new Tile(3, 1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[4][1] = new Tile(4,1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[5][1] = new Tile(5,1,PossibleColors.GREEN);
+
+        host.getShelfie().getShelf()[0][2] = new Tile(0, 2, PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[1][2] = new Tile(1,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[2][2] = new Tile(2,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[3][2] = new Tile(3, 2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[4][2] = new Tile(4,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[5][2] = new Tile(5,2,PossibleColors.YELLOW);
+
+        host.getShelfie().getShelf()[0][3] = new Tile(0, 3, PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[1][3] = new Tile(1,3,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[2][3] = new Tile(2,3,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[3][3] = new Tile(3, 3,PossibleColors.PINK);
+        host.getShelfie().getShelf()[4][3] = new Tile(4,3,PossibleColors.PINK);
+        host.getShelfie().getShelf()[5][3] = new Tile(5,3,PossibleColors.PINK);
+
+        host.getShelfie().getShelf()[0][4] = new Tile(0, 4, PossibleColors.GREEN);
+        host.getShelfie().getShelf()[1][4] = new Tile(1,4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[2][4] = new Tile(2,4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[3][4] = new Tile(3, 4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[4][4] = new Tile(4,4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[5][4] = new Tile(5,4,PossibleColors.PINK);
+
+        if(host.maxValueOfTiles() != 0){
+            fail();
+        }
+
+        host.getShelfie().getShelf()[0][0] = new Tile(0, 0, PossibleColors.BLUE);
+        host.getShelfie().getShelf()[1][0] = new Tile(1,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[2][0] = new Tile(2,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[3][0] = new Tile(3, 0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[4][0] = new Tile(4,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[5][0] = new Tile(5,0,PossibleColors.BLUE);
+
+        host.getShelfie().getShelf()[0][1] = new Tile(0, 1, PossibleColors.BLUE);
+        host.getShelfie().getShelf()[1][1] = new Tile(1,1,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[2][1] = new Tile(2,1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[3][1] = new Tile(3, 1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[4][1] = new Tile(4,1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[5][1] = new Tile(5,1,PossibleColors.GREEN);
+
+        host.getShelfie().getShelf()[0][2] = new Tile(0, 2, PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[1][2] = new Tile(1,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[2][2] = new Tile(2,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[3][2] = new Tile(3, 2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[4][2] = new Tile(4,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[5][2] = new Tile(5,2,PossibleColors.YELLOW);
+
+        host.getShelfie().getShelf()[0][3] = new Tile(0, 3, PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[1][3] = new Tile(1,3,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[2][3] = new Tile(2,3,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[3][3] = new Tile(3, 3,PossibleColors.PINK);
+        host.getShelfie().getShelf()[4][3] = new Tile(4,3,PossibleColors.PINK);
+        host.getShelfie().getShelf()[5][3] = new Tile(5,3,PossibleColors.PINK);
+
+        host.getShelfie().getShelf()[0][4] = new Tile(0, 4, PossibleColors.GREEN);
+        host.getShelfie().getShelf()[1][4] = new Tile(1,4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[2][4] = new Tile(2,4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[3][4] = new Tile(3, 4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[4][4] = new Tile(4,4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[5][4] = new Tile(5,4,PossibleColors.DEFAULT);
+
+        if(host.maxValueOfTiles() != 1){
+            fail();
+        }
+
+        host.getShelfie().getShelf()[0][0] = new Tile(0, 0, PossibleColors.BLUE);
+        host.getShelfie().getShelf()[1][0] = new Tile(1,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[2][0] = new Tile(2,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[3][0] = new Tile(3, 0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[4][0] = new Tile(4,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[5][0] = new Tile(5,0,PossibleColors.BLUE);
+
+        host.getShelfie().getShelf()[0][1] = new Tile(0, 1, PossibleColors.BLUE);
+        host.getShelfie().getShelf()[1][1] = new Tile(1,1,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[2][1] = new Tile(2,1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[3][1] = new Tile(3, 1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[4][1] = new Tile(4,1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[5][1] = new Tile(5,1,PossibleColors.GREEN);
+
+        host.getShelfie().getShelf()[0][2] = new Tile(0, 2, PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[1][2] = new Tile(1,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[2][2] = new Tile(2,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[3][2] = new Tile(3, 2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[4][2] = new Tile(4,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[5][2] = new Tile(5,2,PossibleColors.YELLOW);
+
+        host.getShelfie().getShelf()[0][3] = new Tile(0, 3, PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[1][3] = new Tile(1,3,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[2][3] = new Tile(2,3,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[3][3] = new Tile(3, 3,PossibleColors.PINK);
+        host.getShelfie().getShelf()[4][3] = new Tile(4,3,PossibleColors.PINK);
+        host.getShelfie().getShelf()[5][3] = new Tile(5,3,PossibleColors.PINK);
+
+        host.getShelfie().getShelf()[0][4] = new Tile(0, 4, PossibleColors.GREEN);
+        host.getShelfie().getShelf()[1][4] = new Tile(1,4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[2][4] = new Tile(2,4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[3][4] = new Tile(3, 4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[4][4] = new Tile(4,4,PossibleColors.DEFAULT);
+        host.getShelfie().getShelf()[5][4] = new Tile(5,4,PossibleColors.DEFAULT);
+
+        if(host.maxValueOfTiles() != 2){
+            fail();
+        }
+
+        host.getShelfie().getShelf()[0][0] = new Tile(0, 0, PossibleColors.BLUE);
+        host.getShelfie().getShelf()[1][0] = new Tile(1,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[2][0] = new Tile(2,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[3][0] = new Tile(3, 0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[4][0] = new Tile(4,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[5][0] = new Tile(5,0,PossibleColors.BLUE);
+
+        host.getShelfie().getShelf()[0][1] = new Tile(0, 1, PossibleColors.BLUE);
+        host.getShelfie().getShelf()[1][1] = new Tile(1,1,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[2][1] = new Tile(2,1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[3][1] = new Tile(3, 1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[4][1] = new Tile(4,1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[5][1] = new Tile(5,1,PossibleColors.GREEN);
+
+        host.getShelfie().getShelf()[0][2] = new Tile(0, 2, PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[1][2] = new Tile(1,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[2][2] = new Tile(2,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[3][2] = new Tile(3, 2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[4][2] = new Tile(4,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[5][2] = new Tile(5,2,PossibleColors.YELLOW);
+
+        host.getShelfie().getShelf()[0][3] = new Tile(0, 3, PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[1][3] = new Tile(1,3,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[2][3] = new Tile(2,3,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[3][3] = new Tile(3, 3,PossibleColors.PINK);
+        host.getShelfie().getShelf()[4][3] = new Tile(4,3,PossibleColors.PINK);
+        host.getShelfie().getShelf()[5][3] = new Tile(5,3,PossibleColors.PINK);
+
+        host.getShelfie().getShelf()[0][4] = new Tile(0, 4, PossibleColors.GREEN);
+        host.getShelfie().getShelf()[1][4] = new Tile(1,4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[2][4] = new Tile(2,4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[3][4] = new Tile(3, 4,PossibleColors.DEFAULT);
+        host.getShelfie().getShelf()[4][4] = new Tile(4,4,PossibleColors.DEFAULT);
+        host.getShelfie().getShelf()[5][4] = new Tile(5,4,PossibleColors.DEFAULT);
+
+        if(host.maxValueOfTiles() != 3){
+            fail();
+        }
+
+        host.getShelfie().getShelf()[0][0] = new Tile(0, 0, PossibleColors.BLUE);
+        host.getShelfie().getShelf()[1][0] = new Tile(1,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[2][0] = new Tile(2,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[3][0] = new Tile(3, 0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[4][0] = new Tile(4,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[5][0] = new Tile(5,0,PossibleColors.BLUE);
+
+        host.getShelfie().getShelf()[0][1] = new Tile(0, 1, PossibleColors.BLUE);
+        host.getShelfie().getShelf()[1][1] = new Tile(1,1,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[2][1] = new Tile(2,1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[3][1] = new Tile(3, 1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[4][1] = new Tile(4,1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[5][1] = new Tile(5,1,PossibleColors.GREEN);
+
+        host.getShelfie().getShelf()[0][2] = new Tile(0, 2, PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[1][2] = new Tile(1,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[2][2] = new Tile(2,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[3][2] = new Tile(3, 2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[4][2] = new Tile(4,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[5][2] = new Tile(5,2,PossibleColors.YELLOW);
+
+        host.getShelfie().getShelf()[0][3] = new Tile(0, 3, PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[1][3] = new Tile(1,3,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[2][3] = new Tile(2,3,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[3][3] = new Tile(3, 3,PossibleColors.PINK);
+        host.getShelfie().getShelf()[4][3] = new Tile(4,3,PossibleColors.PINK);
+        host.getShelfie().getShelf()[5][3] = new Tile(5,3,PossibleColors.PINK);
+
+        host.getShelfie().getShelf()[0][4] = new Tile(0, 4, PossibleColors.GREEN);
+        host.getShelfie().getShelf()[1][4] = new Tile(1,4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[2][4] = new Tile(2,4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[3][4] = new Tile(3, 4,PossibleColors.DEFAULT);
+        host.getShelfie().getShelf()[4][4] = new Tile(4,4,PossibleColors.DEFAULT);
+        host.getShelfie().getShelf()[5][4] = new Tile(5,4,PossibleColors.DEFAULT);
+
+        if(host.maxValueOfTiles() != 3){
+            fail();
+        }
+
+        //FRAAAAAA
+
+
 
     }
+    @Test
+    public void ChoosableTilesCrashTest() throws RemoteException {
+        try {
+            host.choosableTiles(0);
+        }catch (IllegalValueException e){
+            assertEquals("Wrong tiles number", e.getMessage());
+        }
+        try {
+            host.choosableTiles(4);
+        }catch (IllegalValueException e){
+            assertEquals("Wrong tiles number", e.getMessage());
+        }
+    }
+
+    @Test
+    public void ChoosableTilesTest() throws RemoteException, IllegalValueException {
+        try {
+            host.choosableTiles(1);
+        }catch (IllegalValueException e){
+            fail();
+        }
+
+        assertNotNull(host.choosableTiles(1));
+
+        try {
+            host.choosableTiles(2);
+        }catch (IllegalValueException e){
+            fail();
+        }
+
+        assertNotNull(host.choosableTiles(2));
+
+        try {
+            host.choosableTiles(3);
+        }catch (IllegalValueException e){
+            fail();
+        }
+
+        assertNotNull(host.choosableTiles(3));
+
+    }
+    @Test
+    public void chooseSelectedTilesTest() throws WrongTileException {
+        host.setTilesNumber(2);
+        List<Tile> choosenList1 = game.getBoard().choosableTiles(3).get(0);
+        try {
+            host.chooseSelectedTiles(choosenList1);
+        }catch (WrongTileException e){
+            System.out.println(e.getMessage());
+            assertEquals("you have to select " + host.getTilesNumber() + " tiles\n", e.getMessage());
+        }
+
+        host.setTilesNumber(1);
+        List<Tile> choosenList2 = new ArrayList<>();
+        choosenList2.add(new Tile(PossibleColors.DEFAULT));
+
+        try{
+            host.chooseSelectedTiles(choosenList2);
+        }catch (WrongTileException e){
+            assertEquals("Tiles of this type can't be chosen", e.getMessage());
+        }
+
+        host.setTilesNumber(1);
+        List<Tile> choosenList3 = new ArrayList<>();
+        choosenList3.add(new Tile(PossibleColors.XTILE));
+
+        try{
+            host.chooseSelectedTiles(choosenList3);
+        }catch (WrongTileException e){
+            assertEquals("Tiles of this type can't be chosen", e.getMessage());
+        }
+
+        host.getShelfie().getShelf()[0][0] = new Tile(0, 0, PossibleColors.BLUE);
+        host.getShelfie().getShelf()[1][0] = new Tile(1,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[2][0] = new Tile(2,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[3][0] = new Tile(3, 0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[4][0] = new Tile(4,0,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[5][0] = new Tile(5,0,PossibleColors.BLUE);
+
+        host.getShelfie().getShelf()[0][1] = new Tile(0, 1, PossibleColors.BLUE);
+        host.getShelfie().getShelf()[1][1] = new Tile(1,1,PossibleColors.BLUE);
+        host.getShelfie().getShelf()[2][1] = new Tile(2,1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[3][1] = new Tile(3, 1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[4][1] = new Tile(4,1,PossibleColors.GREEN);
+        host.getShelfie().getShelf()[5][1] = new Tile(5,1,PossibleColors.GREEN);
+
+        host.getShelfie().getShelf()[0][2] = new Tile(0, 2, PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[1][2] = new Tile(1,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[2][2] = new Tile(2,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[3][2] = new Tile(3, 2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[4][2] = new Tile(4,2,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[5][2] = new Tile(5,2,PossibleColors.YELLOW);
+
+        host.getShelfie().getShelf()[0][3] = new Tile(0, 3, PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[1][3] = new Tile(1,3,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[2][3] = new Tile(2,3,PossibleColors.YELLOW);
+        host.getShelfie().getShelf()[3][3] = new Tile(3, 3,PossibleColors.PINK);
+        host.getShelfie().getShelf()[4][3] = new Tile(4,3,PossibleColors.PINK);
+        host.getShelfie().getShelf()[5][3] = new Tile(5,3,PossibleColors.PINK);
+
+        host.getShelfie().getShelf()[0][4] = new Tile(0, 4, PossibleColors.GREEN);
+        host.getShelfie().getShelf()[1][4] = new Tile(1,4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[2][4] = new Tile(2,4,PossibleColors.PINK);
+        host.getShelfie().getShelf()[3][4] = new Tile(3, 4,PossibleColors.DEFAULT);
+        host.getShelfie().getShelf()[4][4] = new Tile(4,4,PossibleColors.DEFAULT);
+        host.getShelfie().getShelf()[5][4] = new Tile(5,4,PossibleColors.DEFAULT);
+
+        host.setTilesNumber(3);
+        List<Tile> choosableTiles4 = new ArrayList<>();
+        choosableTiles4.add((new Tile(PossibleColors.BLUE)));
+        choosableTiles4.add((new Tile(PossibleColors.BLUE)));
+        choosableTiles4.add((new Tile(PossibleColors.BLUE)));
+
+        boolean[] testC4 = {false, false, false, false, true};
+
+        for(int i=0; i<5; i++) {
+            assertEquals(host.chooseSelectedTiles(choosableTiles4)[i], testC4[i]);
+        }
+    }
+    @Test
+    public void insertTileTest(){
+
+    }
+
 }
