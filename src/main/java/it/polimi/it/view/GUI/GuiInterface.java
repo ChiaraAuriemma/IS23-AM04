187850a3aec557a515fcb8dd85c0cb35a7738140
ChiaraAuriemma <chiara.auriemma@mail.polimi.it>
2023-06-27 10:58:03 +0200

    Gui interface

--- src/main/java/it/polimi/it/view/GUI/GuiInterface.java
+++ src/main/java/it/polimi/it/view/GUI/GuiInterface.java
@@ -1,19 +1,9 @@
 package it.polimi.it.view.GUI;
 
 import it.polimi.it.network.client.ClientInterface;
-import it.polimi.it.network.client.GUIHandler;
-import javafx.stage.Stage;
+
 
 //interfaccia utilizzata per astrarre i controller, tutti i controllere delle scene implementano questi metodi
 public interface GuiInterface {
-    void setReferenceGUI(GUIApplication gui);
-    void setReferenceHandler(GUIHandler handler);
-
     void setClient(ClientInterface client);
-
-    void setStage(Stage stage);
-
-    String getType();
-
-
 }
